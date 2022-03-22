86a7e5f37e1f279a77e1906e7953b640e76be2c3
Charlie Moog <moogcharlie@gmail.com>
2022-03-21 20:08:42 -0500

    dev: use flake to lock devshell

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "VS Code extension for opening SQL files as interactive notebooks.";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          packages = with pkgs; [ nodejs-16_x ];
+        };
+      }
+    );
+}
